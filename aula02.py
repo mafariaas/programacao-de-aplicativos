2aa2f2e9e696a7572057701ffbfb66339d9b010d
Maria Farias <maria.farias01088084@sesisenaipr.org.br>
2026-02-18 12:52:13 -0300

    primeira aula de programação

---
+++ aula02.py
@@ -0,0 +1,9 @@
+numero = 233 # variavel int
+numero_quebrado = 23.3 # variavel float
+celular_esta_desligado = True # variavcel boolena 
+texto = "Aula esta acabando" # variavel string 
+
+print(numero)
+print(numero_quebrado)
+print(celular_esta_desligado)
+print(texto)
