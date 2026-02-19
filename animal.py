244c320883c8c0aa7f11e74102af7e0ac64eee9d
Maria Farias <maria.farias01088084@sesisenaipr.org.br>
2026-02-19 09:51:04 -0300

    aula atividade animal

---
+++ animal.py
@@ -0,0 +1,9 @@
+nome = "gato" # variavel de texto 
+idade = 3 # variavel de numero 
+peso = 2.5 # variavel float 
+sexo = "fêmea" # variavel de texto 
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo)
