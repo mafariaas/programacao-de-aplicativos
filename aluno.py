857e2f24415ca9401d82b16b1ec39e24c2da1db2
Maria Farias <maria.farias01088084@sesisen aipr.org.br>
2026-02-20 09:46:54 -0300

    projeto aluno

---
+++ aluno.py
@@ -0,0 +1,9 @@
+nome = input("digite seu nome: ")
+telefone = int(input("Digite seu telefone: "))
+idade = int(input("Digite sua idade: "))
+cidade = input("Digite o nome da sua cidade: ")
+
+print ("Ola" , nome)
+print ("Sua idade é" , idade)
+print ("Seu número é" , telefone)
+print ("você mora em" , cidade) 
---
+++ matematica.py
@@ -0,0 +1,18 @@
+valor = int(input("digite um valor"))
+valor2 = int(input("digite outro valor"))
+
+print ("seu valor é" , valor)
+print ("seu valor é" , valor)
+
+
+
+
+soma = valor + valor2
+subtracao = valor - valor2 
+multiplicacao = valor * valor2
+divisao = valor / valor2
+
+print ("o resultado da soma é" , soma)
+print ("o resultado da subtracao é" , subtracao)
+print ("o resultado da multiplicacao é" , multiplicacao)
+print ("o resultado da divisao é" , divisao)
