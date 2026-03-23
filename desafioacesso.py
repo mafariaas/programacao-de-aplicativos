e35e017fed5f02812fec519aee29e32a3d413e3b
Maria <maria.farias01088084@sesisenaipr.org.br>
2026-03-23 12:46:44 -0300

    23/03

--- desafioacesso.py
+++ desafioacesso.py
@@ -1,4 +1,14 @@
 autorizados = ["Alice", "Bob", "Carlos"]
 nome = input("Digite o nome de um pesquisador: ")
-if nome == True:
-    print(f"Acesso Permitido! O pesquisador {nome} está na posição {indice}", autorizados.index())
+if nome == "Alice" or "Bob" or "Carlos":
+    print(f"Acesso Permitido! O pesquisador {nome} está na posição", autorizados.index(nome))
+    pergunta = input ("Deseja remover esse pesquisador da lista (Sim/Não)")
+if pergunta == "Sim":
+    autorizados.remove(nome)
+    print(f"Agora os membros da lista são {autorizados}")
+else:
+    print(f"Acesso negado! O pesquisador {nome} não foi encontrado")
+    pergunta2 = input("Deseja cadastrar esse nome como novo pesquisador? (Sim/Não): ")
+    if pergunta2 == "Sim":
+        autorizados.append(nome)
+
