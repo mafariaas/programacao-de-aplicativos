ce32071f2c26778b43eb85e58f9d07beb107a6df
Maria <maria.farias01088084@sesisenaipr.org.br>
2026-04-08 11:59:27 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ esvaziarpilha.py
@@ -0,0 +1,4 @@
+objetos = ["corda", "martelo", "livro", "caderno"]
+while len(objetos) > 0:
+    remove = objetos.pop(0)
+    print(f"Objetos removidos", objetos)
---
+++ trocavalores.py
@@ -0,0 +1,9 @@
+numeros_A = input("digite o numero A: ")
+numeros_B = input("digite o numero B: ")
+c = 0 
+
+c = numeros_A 
+numeros_A = numeros_B
+numeros_B = c 
+
+print("Números", numeros_A, numeros_B)
