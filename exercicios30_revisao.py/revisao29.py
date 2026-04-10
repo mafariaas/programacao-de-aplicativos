eea806fced7c5c2aae52dd327f33d362b29a6ee4
M <maria.farias01088084@sesisenaipr.org.br>
2026-04-09 22:46:56 -0300

    Revisao30

--- exercicios30_revisao.py/revisao29.py
+++ exercicios30_revisao.py/revisao29.py
@@ -2,9 +2,12 @@ salarios = [2000, 2750, 4400, 1800, 1000]
 for s in salarios:
     if s <= 2000.00:
         imposto = s * 0.10
+        sobra = s - imposto
         percentual = "10%"
-    else:
+    elif s > 2000:
         imposto = s * 0.20
-    sobra = s - imposto
+        sobra = s - imposto
+        percentual = "20%"
 
-    print(f"salário {s}, e foi cobrado {percentual}, de imposto")
+
+    print(f"O Salário {s} foi cobrado {percentual} de imposto")
