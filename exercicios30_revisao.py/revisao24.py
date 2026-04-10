701895e1885c6dfbb5eb77248452c474656d7482
M <maria.farias01088084@sesisenaipr.org.br>
2026-04-09 21:54:55 -0300

    Revisao30

--- exercicios30_revisao.py/revisao24.py
+++ exercicios30_revisao.py/revisao24.py
@@ -1,5 +1,26 @@
-numeros = [23, 3, 20, 10]
-print("A lista de números é:",numeros)
-print("-------------------------")
-numeros[3] = numeros[0] + numeros[1]
-print("A lista nova é:",numeros)
+saldo = 500
+opcao = 0 
+while opcao != "3":
+    print("--Caixa eletrônico--")
+    print("1- Depositar")
+    print("2- Sacar")
+    print("3- Sair")
+   
+    opcao = input("Digite a opção desejada: ")
+
+    if opcao == "1":
+        depositar = float(input("Qual valor deseja depositar?: R$"))
+        saldo = saldo + depositar
+        print(f"Depósito realizado! Saldo atual: R${saldo}")
+
+    if opcao == "2":
+        saque = float(input("Qual o valor deseja sacar?: R$"))
+        
+        if saque <= saldo:
+            saldo = saldo - saque
+            print(f"Saque realizado! Novo saldo: R${saldo}")
+        else:
+            print("Operação negada! Saldo insuficiente.")
+
+
+
