c4f85f63a2a275055fde5cfc0088c49ba3ca8ff2
M <maria.farias1088084@sesisenaipr.org.br>
2026-06-15 12:45:08 -0300

    15/06

--- 2 TRI/ex2_sqli3.py
+++ 2 TRI/ex2_sqli3.py
@@ -1,40 +1,26 @@
 import sqlite3
-conexao = sqlite3.connect('escola.demonstracao.db')
+
+conexao = sqlite3.connect('escola_demonstracao.db')
 cursor = conexao.cursor()
 
-try:
-    print("\n--- ALUNOS CADASTRADOS ---")
-    cursor.execute("SELECT * FROM alunos;")
-    linhas = cursor.fetchall()
-    
-    if not linhas:
-        print("Nenhum aluno cadastrado ainda.")
-    else:
-        for linha in linhas:
-            print(f"ID: {linha[0]} | Nome: {linha[1]} | CPF: {linha[5]}")
+id_aluno = int(input("Digite o ID do aluno que deseja alterar: "))
+novo_nome = input("Digite o novo nome: ")
+novo_cpf = input("Digite o novo CPF: ")
 
-        print("\n--- ALTERAÇÃO DE DADOS ---")
-        id_alterar = int(input("Digite o ID do aluno que deseja alterar: "))
-   
-        cursor.execute("SELECT id FROM alunos WHERE id = ?", (id_alterar,))
-        if cursor.fetchone() is None:
-            print("Erro: ID não encontrado no banco de dados.")
-        else:
-            novo_nome = input("Digite o novo nome do aluno: ")
-            novo_cpf = input("Digite o novo CPF do aluno: ")
+sql = f'''
+UPDATE Alunos
+SET nome = '{novo_nome}',
+    cpf = '{novo_cpf}'
+WHERE id = {id_aluno}
+'''
 
-            comando_alterar = '''
-                UPDATE alunos 
-                SET nome_aluno = ?, cpf_aluno = ? 
-                WHERE id = ?
-            '''
-            
-            cursor.execute(comando_alterar, (novo_nome, novo_cpf, id_alterar))
+cursor.execute(sql)
 
-            conexao.commit()
-            print(f"\nSucesso! Os dados do aluno com ID {id_alterar} foram atualizados.")
+conexao.commit()
 
-except sqlite3.IntegrityError:
-    print("\nErro: Este CPF já está cadastrado para outro aluno (o CPF deve ser único).")
-except ValueError:
-    print("\nErro: Por favor, digite um número válido para o ID.")
+if cursor.rowcount > 0:
+    print("Aluno atualizado com sucesso!")
+else:
+    print("Nenhum aluno encontrado com esse ID.")
+
+conexao.close()
