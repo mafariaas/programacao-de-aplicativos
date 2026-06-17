44a5c59787bc4a1a77409dd658b648fbc1a2fb60
M <maria.farias01088084@sesisenaipr.org.br>
2026-06-17 12:34:24 -0300

    17/06

--- 2 TRI/cadastro_profesores.py
+++ 2 TRI/cadastro_profesores.py
@@ -19,16 +19,15 @@ def criar_tabela():
     materia_professor = input("Informe a matéria do professor: ")
     idade_professor = int(input("Digite a idade do professor: "))
     cpf_professor = input("Digite o cpf do professor: ")
-    salario = float(input("Digite o salário do professor: "))
+    salario_professor = float(input("Digite o salário do professor: "))
     nome_escola = input("Informe o nome da escola: ")
     
     comando_inserir = (f'''
                     INSERT INTO professores (nome, telefone, materia, idade, cpf, salario, escola)
-                    VALUES ('{nome_professor}', '{telefone_professor}', '{materia_professor}', '{idade_professor}', '{cpf_professor}', '{salario_professor}', '{nome_escola}')''')
+                    VALUES ('{nome_professor}', '{telefone_professor}', '{materia_professor}', {idade_professor}, '{cpf_professor}', {salario_professor}, '{nome_escola}')''')
 
-cursor.execute(comando_inserir)
-conexao.commit()
-conexao.close() 
+    cursor.execute(comando_inserir)
+    conexao.commit()
 
 def listar():
     cursor.execute("SELECT * FROM professores")
@@ -41,7 +40,7 @@ def listar():
         print("Nenhum professore cadastrado!")
 
     else:
-        for prof in todos_professores:
+        for professor in todos_professores:
             print(f"ID: {professor[0]}")
             print(f"Nome: {professor[1]}")
             print(f"Telefone: {professor[2]}")
@@ -52,4 +51,70 @@ def listar():
             print(f"Nome escola: {professor[7]}")
             print("-" * 30)
 
-    conexao.close()
+
+def alterar():
+    listar()
+    id_professor = int(input("Digite o ID do professor que deseja alterar: "))
+    novo_nome = input("Digite o novo nome: ")
+    novo_cpf = input("Digite o novo CPF: ")
+
+    sql = f'''
+    UPDATE Professores
+    SET nome = '{novo_nome}',
+        cpf = '{novo_cpf}'
+    WHERE id = {id_professor}
+    '''
+
+    cursor.execute(sql)
+
+    conexao.commit()
+
+    if cursor.rowcount > 0:
+        print("Professor atualizado com sucesso!")
+    else:
+        print("Nenhum professor encontrado com esse ID.")
+
+
+def excluir():
+    listar()
+    id_professor = int(input("Digite o ID do professor que deseja excluir: "))
+
+    sql = f"DELETE FROM Professores WHERE id = {id_professor}"
+
+    cursor.execute(sql)
+    conexao.commit()
+
+    if cursor.rowcount > 0:
+        print("Professor excluído com sucesso!")
+    else:
+        print("Nenhum professor encontrado com esse ID.")
+
+
+def menu():
+    opcao = 0
+    while opcao != 5:
+        print("\n---CADASTRO DE PROFESSORES---")
+        print("\n1-Criar")
+        print("2-Listar")
+        print("3-Alterar")
+        print("4-Excluir")
+        print("5-Sair")
+
+        opcao = int(input("\nDigite a opção desejada: "))
+
+        if opcao == 1: criar_tabela()
+        elif opcao == 2: listar()
+        elif opcao == 3: alterar()
+        elif opcao == 4: excluir()
+        elif opcao == 5:
+            conexao.close()
+            print("Programa encerrado!")
+            break
+
+menu()
+
+        
+
+
+
+
