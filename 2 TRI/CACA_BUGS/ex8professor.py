38be62ab47753b5712c9e0a2741458990ceb1480
M <maria.farias01088084@sesisenaipr.org.br>
2026-07-01 09:26:35 -0300

    01/07

--- 2 TRI/CACA_BUGS/ex8professor.py
+++ 2 TRI/CACA_BUGS/ex8professor.py
@@ -1,3 +1,15 @@
 import sqlite3
 
-def cadastrar_professores
+def cadastrar_professores(nome, cpf):
+    conexao = sqlite3.connect('sistema_escola.db')
+    cursor = conexao.cursor()
+
+    # O sistema aceita cadastrar dois professores com o mesmo CPF.
+    # Como restringir isso direto na estrutura da tabela abaixo?
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS professores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT
+            cpf TEXT
+        )
+''')
