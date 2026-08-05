147335c504f2fe86629e2ec6dff04ddcb581b617
M <maria.farias01088084@sesisenaipr.org.br>
2026-08-05 09:24:35 -0300

    05/08

--- 2 TRI/CACA_BUGS/prova_2_trimestre.py
+++ 2 TRI/CACA_BUGS/prova_2_trimestre.py
@@ -11,17 +11,28 @@ def cadastrar_hopital():
         cidade TEXT NOT NULL
         )
     ''')
+    try:
+        nome_hospital = input("Informe o nome do hospital: ")
+        cidade_loc = input("Informe a cidade em que o hospital está localizado: ")
+        comando_inserir = "INSERT INTO hospitais (nome, cidade) VALUES (?, ?)"
+                           
+                            
+        print("Hospital cadastrado!")
+        conexao.commit()
+    except ValueError as e:
+        print("Digite apenas nomes! ", e)
+    except sqlite3.IntegrityError as e:
+        print("Erro! Informações já cadastradas! ", e)
 
-    nome_hospital = input("Informe o nome do hospital: ")
-    cidade_loc = input("Informe a cidade em que o hospital está localizado: ")
-    print("Hospital cadastrado!")
-
-    cursor.execute("INSERT INTO hospitais")
+    finally:
+        conexao.close()
 
 def cadastrar_medicos():
     conexao = sqlite3.connect('sistema_hospital.db')
     cursor = conexao.cursor() 
-     
+    
+    cursor.execute("PRAGMA foreign_keys = ON")
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS medicos (
         id INTEGER UNIQUE PRIMARY KEY,
@@ -31,9 +42,26 @@ def cadastrar_medicos():
         FOREIGN KEY (id_hospital) REFERENCES hospitais(id)
         )
         ''')
+    try:
+        nome_medico = input("Informe o nome do médico: ")
+        crm = int(input("Informe o crm do médico: "))
+        id_hospital = int(input("Informe o id do hospital em que o médico trabalha: "))
+        comando_inserir = (f'''
+                            (INSERT INTO medicos (nome, crm, id_hospital) )
+                            VALUES ('{nome_medico}', '{crm}', '{id_hospital}')''')
 
-    nome_medico = input("Informe o nome do médico: ")
-    crm = int(input("Informe o crm do médico: "))
-    id_hospital = int(input("Informe o id do hospital em que o médico trabalha: "))
+        print("Médico cadastrado com sucesso!")
+        conexao.commit()
+
+    except ValueError as e:
+        ("Digite apenas números!", e)
+    
+    except sqlite3.IntegrityError as e:
+        ("Erro! Informações já cadastradas! ", e)
+
+    finally:
+        conexao.close()
 
 
+cadastrar_hopital()
+cadastrar_medicos()
